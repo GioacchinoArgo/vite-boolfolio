1f41636643e63b260c5fc03ff6508205b31f57e9
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-01 02:10:25 +0200

    added AppHeader.vue and projects title on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
+
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
 </script>
 
 <template>
+  <AppHeader />
+  <main class="container pt-4">
+    <h1>Boolfolio</h1>
 
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h4 v-text="project.title"></h4>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <style scoped></style>
